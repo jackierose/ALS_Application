6b53d10c150b047a0de52d05df5b78722dc2e4ee
Arjun Bagla <arjunbagla3695@gmail.com>
2017-07-08 08:50:29 -0700

    Good build working fine

--- app/src/main/java/paaj/als_application/BuildStoryActivity.java
+++ app/src/main/java/paaj/als_application/BuildStoryActivity.java
@@ -2,24 +2,159 @@ package paaj.als_application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class BuildStoryActivity extends AppCompatActivity {
+import com.affectiva.android.affdex.sdk.Frame;
+import com.affectiva.android.affdex.sdk.detector.CameraDetector;
+import com.affectiva.android.affdex.sdk.detector.Detector;
+import com.affectiva.android.affdex.sdk.detector.Face;
+
+import java.util.List;
+
+
+
+public class BuildStoryActivity extends AppCompatActivity implements Detector.ImageListener {
+
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    int x,y,z = 0;
+    public char[][] layer = {{'a', 'b', 'c','a', 'b', 'c','a', 'b', 'c','a'},
+                            {'d', 'e', 'f', 'g', 'h','d', 'e', 'f', 'g', 'h'},
+                            {'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','i', 'j'} ,
+                            {'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}};
+    public char[] layer1 = {'a', 'b', 'c'};
+    public char[] layer2 = {'d', 'e', 'f', 'g', 'h'};
+    public char[] layer3 = {'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'};
+    public char[] layer4 = {'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+    String[] words = {"my", "as", "the", "is", "foo", "name", "bar"};
+
+    public int state = 0;
+    boolean direction = false;
+    boolean imageListnerController = true;
+
+    CameraDetector detector;
+    SurfaceView cameraPreview;
+
+    TextView help;
+    TextView help2;
+
+    String curr_char = "";
+    String resultArray = "";
+    String result = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_build_story);
+
+        intializeUI();
+        detector.start();
+
+        //keyboard_layer();
+    }
+
+
+    private void intializeUI() {
+
+        cameraPreview = (SurfaceView)findViewById(R.id.camerapreview);
+        detector = new CameraDetector(this, CameraDetector.CameraType.CAMERA_FRONT, cameraPreview);
+        detector.setImageListener(this);
+        detector.setDetectSmile(true);
+        detector.setDetectEyeClosure(true);
+        detector.setDetectMouthOpen(true);
+        detector.setDetectBrowRaise(true);
+        detector.setDetectLipPress(true);
+        detector.setMaxProcessRate(10);
+        help = (TextView)findViewById(R.id.help);
+        help2 = (TextView)findViewById(R.id.help2);
     }
 
     protected void sendText(View view) {
-        EditText editText = (EditText) findViewById(R.id.editText2);
-        String message = editText.getText().toString();
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(message);
+       // resultArray += curr_char;
+        Log.d("mytag", "RES ==== " + resultArray + "\n");
+        x = 0;
+        y = 0;
+
+        if (result.length() == 0) {
+            result += words[z];
+        } else {
+            result += " " + words[z];
+        }
+        help2.setText(result);
+        z = 0;
+        //imageListnerController = true;
+    }
+
+
+
+int count = 0;
+
+    @Override
+    public void onImageResults(List<Face> faces, Frame frame, float v) {
+
+        help.setText(words[z]);
+        help2.setText(result);
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        if (faces.size() == 0) {
+
+        } else {
+            count++;
+            Log.d("mytag", "Ps55555ttt ==== \n");
+           if (count % 3 == 0) {
+               Log.d("mytag", "Psttt ==== \n");
+                Face face = faces.get(0);
+
+                if (face.expressions.getBrowRaise() > 80) {
+                   // imageListnerController = false;
+                    Log.d("mytag", "brow\n");
+                    moveRight();
+
+            //        imageListnerController = false;
+                } else if (face.expressions.getSmile() > 80) {
+                   // imageListnerController = false;
+                    sendText(null);
+                    state = 0;
+          //          imageListnerController = false;
+                } else if (face.expressions.getEyeClosure() > 80) {
+                    Log.d("mytag", "eye\n");
+                    //  imageListnerController = false;
+                    moveLeft();
+         //           imageListnerController = false;
+                }
+
+           }
+
+        }
+    }
+
+    public void moveRight() {
+        z++;
+        if (z >= words.length) {
+            z = 0;
+        }
+
+        help.setText(words[z]);
+        help2.setText(result);
+    }
+
+    public void moveLeft() {
+        z--;
+        if (z < 0) {
+            z = words.length - 1;
+        }
+x
+        help.setText(words[z]);
+        help2.setText(result);
     }
 
 }
--- app/src/main/java/paaj/als_application/MainPage.java
+++ app/src/main/java/paaj/als_application/MainPage.java
@@ -3,16 +3,55 @@ package paaj.als_application;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainPage extends AppCompatActivity {
+import com.affectiva.android.affdex.sdk.Frame;
+import com.affectiva.android.affdex.sdk.detector.CameraDetector;
+import com.affectiva.android.affdex.sdk.detector.Detector;
+import com.affectiva.android.affdex.sdk.detector.Face;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class MainPage extends AppCompatActivity implements Detector.ImageListener {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    CameraDetector detector;
+    SurfaceView cameraPreview;
+    TextView textview;
+
+    boolean movedFromView = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+        intializeUI();
+        detector.start();
     }
+
+    private void intializeUI() {
+
+        textview = (TextView)findViewById(R.id.help);
+        cameraPreview = (SurfaceView)findViewById(R.id.cameraview);
+        detector = new CameraDetector(this, CameraDetector.CameraType.CAMERA_FRONT, cameraPreview);
+        detector.setImageListener(this);
+        detector.setDetectSmile(true);
+        detector.setDetectEyeClosure(true);
+        detector.setDetectJawDrop(true);
+        detector.setDetectBrowRaise(true);
+        detector.setDetectChinRaise(true);
+        detector.setMaxProcessRate(10);
+
+        textview.setTranslationX(0);
+    }
+
+
+
 
     public void moveToWrite(View view) {
         Intent intent = new Intent(this, BuildStoryActivity.class);
@@ -26,5 +65,45 @@ public class MainPage extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        movedFromView = false;
+    }
 
+    @Override
+    public void onImageResults(List<Face> faces, Frame frame, float v) {
+        String result = "";
+        if (faces.size() == 0) {
+            textview.setText("No Face!" + movedFromView);
+        } else {
+            Face face = faces.get(0);
+            if (face.expressions.getSmile() > 60) {
+
+                if (!movedFromView) {
+                    movedFromView = true;
+                    detector.stop();
+                    moveToWrite(null);
+                }
+
+
+            } else if (face.expressions.getBrowRaise() > 60) {
+                if (!movedFromView) {
+                    movedFromView = true;
+                    detector.stop();
+                    moveToRead(null);
+                }
+
+            }
+            result = String.format("S: %.2f, ic = %.2f, jd = %.2f, br = %.2f, cn = %.2f",
+                    face.expressions.getSmile(),
+                    face.expressions.getEyeClosure(),
+                    face.expressions.getJawDrop(),
+                    face.expressions.getBrowRaise(),
+                    face.expressions.getChinRaise());
+
+            textview.setText(result);
+        }
+
+    }
 }
